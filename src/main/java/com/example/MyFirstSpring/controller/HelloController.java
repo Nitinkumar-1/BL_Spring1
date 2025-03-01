35054f6141058fdacf5ce853c12c9e85b61e9964
Nitinkumar-1 <nitinkumar766823@gmail.com>
2025-03-01 16:14:59 +0530

    hello

--- src/main/java/com/example/MyFirstSpring/controller/HelloController.java
+++ src/main/java/com/example/MyFirstSpring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/hello")
@@ -13,8 +14,15 @@ public class HelloController {
     public String sayHello() {
         return "Hello from BridgeLabz";
     }
+
     @GetMapping("/query")
     public String sayHello(@RequestParam String name) {
         return "Hello " + name + " from BridgeLabz";
     }
+    @GetMapping("/param/{name}")
+    public String sayHellopath(@PathVariable String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
 }
+
+
